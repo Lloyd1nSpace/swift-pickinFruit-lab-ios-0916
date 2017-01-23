d3c724269b63661dd510e9b009aa34d100c22756
Lloyd1nSpace <lloyd1nspace@icloud.com>
2017-01-23 17:14:03 -0500

    Lab complete

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -24,20 +24,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func spinButtonTapped(_ sender: UIButton) {
-
-        fruitPicker.selectRow(<#T##row: Int##Int#>, inComponent: <#T##Int#>, animated: true)
-        fruitPicker.selectedRow(inComponent: <#T##Int#>)
-        
-        resultLabel.text = "TRY AGAIN"
-        resultLabel.text = "WINNER"
-        
+        fruitPicker.selectRow(Int(arc4random_uniform(UInt32(fruitsArray.count - 1))), inComponent: 0, animated: true)
+        fruitPicker.selectRow(Int(arc4random_uniform(UInt32(fruitsArray.count - 1))), inComponent: 1, animated: true)
+        fruitPicker.selectRow(Int(arc4random_uniform(UInt32(fruitsArray.count - 1))), inComponent: 2, animated: true)
+        updateLabel()
+    }
+ 
+    func updateLabel() {
+        let firstComponent = fruitPicker.selectedRow(inComponent: 0)
+        let secondComponent = fruitPicker.selectedRow(inComponent: 1)
+        let thirdComponent = fruitPicker.selectedRow(inComponent: 2)
+        resultLabel.text = (firstComponent, secondComponent) == (secondComponent, thirdComponent) ? "WINNER!" : "TRY AGAIN"
     }
     
 }
 
 // MARK: Set Up
 extension ViewController {
-    
     override func viewDidLayoutSubviews() {
         if spinButton.layer.cornerRadius == 0.0 {
             configureButton()
@@ -50,40 +53,23 @@ extension ViewController {
         spinButton.layer.borderWidth = 4.0
         spinButton.clipsToBounds = true
     }
-    
 }
 
 // MARK: Picker View Delegate & Data Source
 extension ViewController: UIPickerViewDataSource {
-    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 3
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 1
+        return fruitsArray.count
     }
-    
 }
 
 extension ViewController: UIPickerViewDelegate {
-    
-//    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
-//        
-//    }
-//    
-//    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
-//        
-//    }
-    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return fruitsArray[row]
     }
-    
-//    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-//        
-//    }
-    
 }
 
 
